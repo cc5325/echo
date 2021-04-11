a80ffa83c94ce06c2436fee2e4cc9e5a6b2ed513
Eduardo <e.git@xor.cl>
2021-04-11 19:40:37 -0400

    add a message when server reaches timeout

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"os"
@@ -13,7 +14,12 @@ func Handle(conn net.Conn) {
 		msg := make([]byte, 4096)
 		n, err := conn.Read(msg)
 		if err != nil {
-			log.Printf("error with connection %s: %s", conn.RemoteAddr(), err)
+			if errors.Is(err, os.ErrDeadlineExceeded) {
+				conn.Write([]byte("too slow!\n"))
+				conn.Write([]byte("bye!\n"))
+			} else {
+				log.Printf("error with connection %s: %s", conn.RemoteAddr(), err)
+			}
 			conn.Close()
 			return
 		}
@@ -23,14 +29,19 @@ func Handle(conn net.Conn) {
 }
 
 func main() {
-	s, err := net.Listen("tcp", os.Getenv("ECHO_ADDRESS"))
+	addr := os.Getenv("ECHO_ADDRESS")
+	if len(addr) == 0 {
+		addr = "0.0.0.0:5325"
+	}
+	s, err := net.Listen("tcp", addr)
 	if err != nil {
 		panic(err)
 	}
+	log.Printf("Listening in %s", addr)
 	for {
 		conn, err := s.Accept()
 		if err != nil {
-			log.Printf("error accepting connection: %s", err)
+			log.Printf("Error accepting connection: %s", err)
 		}
 		go Handle(conn)
 	}
